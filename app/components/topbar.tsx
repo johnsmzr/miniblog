216e30b384473c0b8b260fad9e91ee32ddcb89ae
john <73786528+johnsmzr@users.noreply.github.com>
2023-12-25 00:23:48 +0100

    add home link and navItem link for topbar

--- app/components/topbar.tsx
+++ app/components/topbar.tsx
@@ -1,14 +1,57 @@
 "use client";
 
+import clsx from "clsx";
 import Link from "next/link";
 import { useWindowScroll } from "react-use";
+
+const navItems = [
+  { children: "Category", href: "/category" },
+  { children: "About", href: "/about" },
+  { children: "Github", href: "https://github.com" },
+];
 
 export default function Topbar() {
   const { y } = useWindowScroll();
   return (
-    <nav>
-      <div>
-        <Link href="/">Home</Link>
+    <nav
+      className={clsx(
+        "sticky top-0 z-50 h-16",
+        y >= 0 && "bg-white dark:bg-neutral-900 shadow-md"
+      )}
+    >
+      <div
+        className={clsx(
+          "flex justify-between items-center",
+          "max-w-4xl h-full m-auto",
+          "font-medium text-xl tracking-tighter"
+        )}
+      >
+        {/* home link */}
+        <div className="flex-none">
+          <Link
+            className={clsx(
+              "border-b-2 border-transparent hover:border-current"
+            )}
+            href="/"
+          >
+            Home
+          </Link>
+        </div>
+
+        {/* nav items link */}
+        <div
+          className={clsx("flex-1 max-w-xl", "md:flex justify-end", "hidden")}
+        >
+          {navItems.map((navItems) => (
+            <Link
+              className={clsx(
+                "mr-10",
+                "border-b-2 border-transparent hover:border-current"
+              )}
+              {...navItems}
+            />
+          ))}
+        </div>
       </div>
     </nav>
   );
