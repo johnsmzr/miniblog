66b8f0346600b5394ab374b164ebc2feb3c248ca
john <73786528+johnsmzr@users.noreply.github.com>
2023-12-25 22:20:48 +0100

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 // Static metadata
 export const metadata: Metadata = {
-  title: { default: "Capybara Blog", template: "%s | Capybara Blog" },
+  title: {
+    default: "Capybara Blog",
+    template: "%s | Capybara Blog",
+  },
   description: "Generated for All Capybaras in the World",
 };
 
@@ -20,14 +23,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN" className="scroll-smooth">
-      <body className={inter.className}>
+      <body
+        className={clsx(
+          "antialiased m-auto box-border dark:text-white/90 transition-colors duration-300",
+          inter.className
+        )}
+      >
         <ThemeProvider>
           <Topbar />
           <main
             className={clsx(
               "md:max-w-4xl mt-5 m-auto pb-12 px-5 md:px-0",
               "min-h-[calc(100%-4rem)]",
-              "box-border"
+              "box-border",
             )}
           >
             {children}
