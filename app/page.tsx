dc0bd65c4fb405b447ed58dabb0447576ca5257a
john <73786528+johnsmzr@users.noreply.github.com>
2023-12-31 02:45:52 +0100

    add compareTime as function

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import { allBlogs } from "contentlayer/generated";
+import { Blog, allBlogs } from "contentlayer/generated";
 import { BlogCard } from "./components/blog-card";
+
+// 对博客文章按时间进行排序
+export function compareTime(a: Blog, b: Blog) {
+  if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
+    return -1;
+  }
+  return 1;
+}
 
 export default function Home() {
   return (
     <section>
       {allBlogs
-        .sort((a, b) => {
-          if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-            return -1;
-          }
-          return 1;
-        })
+        .sort((a, b) => compareTime(a, b))
         .map((item) => (
           <BlogCard key={item._id} {...item} />
         ))}
