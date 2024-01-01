55a5aaa24308fea4e0008315f18c8c817c839e2e
john <73786528+johnsmzr@users.noreply.github.com>
2024-01-01 20:52:21 +0100

    add remarkGfm

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,40 +1,38 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
-import rehypePrettyCode from 'rehype-pretty-code'
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypePrettyCode from "rehype-pretty-code";
+import remarkGfm from "remark-gfm";
 
 export const Blog = defineDocumentType(() => ({
-  name: 'Blog',
-  filePathPattern: '**/*.mdx',
-  contentType: 'mdx',
+  name: "Blog",
+  filePathPattern: "**/*.mdx",
+  contentType: "mdx",
   fields: {
     title: {
-      type: 'string',
+      type: "string",
       required: true,
     },
     summary: {
-      type: 'string',
+      type: "string",
       required: true,
     },
     publishedAt: {
-      type: 'string',
+      type: "string",
       required: true,
     },
   },
   computedFields: {
     slug: {
-      type: 'string',
+      type: "string",
       resolve: (doc) => doc._raw.flattenedPath,
     },
   },
-}))
+}));
 
 export default makeSource({
-  contentDirPath: 'content',
+  contentDirPath: "content",
   documentTypes: [Blog],
   mdx: {
-    rehypePlugins: [
-      [rehypePrettyCode, { theme: 'github-dark' }],
-      // [rehypeImgSize, { dir: 'public' }],
-    ],
-    // remarkPlugins: [remarkGfm, remarkExternalLinks],
+    rehypePlugins: [[rehypePrettyCode, { theme: "github-dark" }]],
+    remarkPlugins: [remarkGfm],
   },
-})
+});
