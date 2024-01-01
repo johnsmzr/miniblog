c48f38e44fcacae201c42b7898ccb4c0bc9e8d50
john <73786528+johnsmzr@users.noreply.github.com>
2024-01-01 21:34:23 +0100

    clean code by adding buttonUI as const

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -2,7 +2,7 @@
 
 import clsx from "clsx";
 import Link from "next/link";
-import { useWindowScroll } from "react-use";
+
 import SwitchTheme from "./SwitchTheme";
 import Image from "next/image";
 
@@ -24,7 +24,12 @@ const navItems = [
 ];
 
 export default function TopBar() {
-  const { y } = useWindowScroll();
+  const buttonUI =
+    "mr-10 px-4 py-2 \
+    rounded-md transform duration-300 \
+    hover:scale-110 hover:bg-gray-200 \
+    dark:hover:bg-gray-700 hover:bg-gray-200";
+
   return (
     <nav className="flex items-center justify-between py-6 px-2 text-2xl">
       {/* home */}
@@ -39,22 +44,18 @@ export default function TopBar() {
           height={80}
           className=" rounded-full overflow-hidden mr-2 hover:opacity-90"
         />
-        <span className="mr-10 px-4 py-2 hover:scale-110 hover:bg-gray-200 rounded-md transform duration-300">Home</span>
+        <span className={buttonUI}>Home</span>
       </Link>
 
       {/* nav items link */}
       <div className="flex-1 max-w-xl md:flex justify-end hidden">
         {navItems.map((navItems) => (
-          <Link
-            key={navItems.title}
-            className="mr-10 px-4 py-2 hover:scale-110 hover:bg-gray-200 rounded-md transform duration-300"
-            {...navItems}
-          >
+          <Link key={navItems.title} className={buttonUI} {...navItems}>
             {navItems.title}
           </Link>
         ))}
         {/* change dark/light mode */}
-        <SwitchTheme className="mr-10 px-4 py-2 hover:scale-110 hover:bg-gray-200 rounded-md transform duration-300"/>
+        <SwitchTheme className={buttonUI} />
       </div>
     </nav>
   );
