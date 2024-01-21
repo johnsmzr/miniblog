03e2ce70c8d38355dc250785c42b2000b303c041
john <73786528+johnsmzr@users.noreply.github.com>
2024-01-21 21:19:15 +0100

    add code to generate robots.txt and sitemap.xml

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://miniblog-lake.vercel.app/sitemap.xml',
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from "next";
+import { Blog, allBlogs } from "contentlayer/generated";
+
+// 对博客文章按时间进行排序
+function compareTime(a: Blog, b: Blog) {
+  if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
+    return -1;
+  }
+  return 1;
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const allPost = [...allBlogs]
+    .sort((a, b) => compareTime(a, b))
+    .map((item) => ({
+      url: `https://https://miniblog-lake.vercel.app/${item.url}`,
+      lastModified: new Date(item.publishedAt),
+    }));
+
+  return [
+    {
+      url: "https://miniblog-lake.vercel.app/",
+      lastModified: new Date(),
+    },
+    ...allPost,
+  ];
+}
